ebe036228d3a2d024672fb47b586e5c6ba2b356b
Luca De Feo <dfl@deagol>
2013-03-12 01:27:31 +0100

    - Wrapped mysql and node-sqlite-purejs - Slight refactoring of the interface

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var cons = require('consolidate');
 var path = require('path');
+var mysql = require('mysql');
+var sqlite = require('node-sqlite-purejs');
+var SqlString = require('mysql/lib/protocol/SqlString');
 
 /* Substitute exports with createServer */
 exports = module.exports = createServer;
@@ -11,59 +14,131 @@ exports.createServer = createServer;
 exports.express = express;
 exports.connect = express;
 
-/* Some hardcoded configuration */
-var stat_dir = path.resolve(require.main.filename, '../static');
-var stat_mount = '/static';
-var tmpl_dir = path.resolve(require.main.filename, '../templates');
-var sep = '---------------------------------';
+/* Some default configuration */
+function Options(opt) {
+    this.static_dir = opt.static_dir || path.resolve(require.main.filename, '../static');
+    this.static_mount = opt.static_mount || '/static';
+    this.template_dir = opt.template_dir || path.resolve(require.main.filename, '../templates');
+    this.default_handler = opt.default_handler || exports.not_found_handler;
+    this.separator = opt.separator || '---------------------------------';
+    this.secret = opt.secret || 'WAS';
+    this.port = opt.port || 8080;
+    this.db = opt.db;
+}
 
 /* 
    This function creates an express app.
-   The optional secret is used for session cookies.
 */
-function createServer(secret) {
+function createServer(options) {
+    var opt = new Options(options || {});
+
     var app = express();
+    // Import configuration
+    for (var o in opt) {
+	app.set(o, opt[o]);
+    }
     // Default middleware
     app.use(express.favicon())
 	.use(express.query())
 	.use(express.bodyParser())
 	.use(express.cookieParser())
-	.use(express.session( {secret: secret || 'WAS' } ))
-	.use(stat_mount, express.static(stat_dir))
+	.use(express.session( {secret: opt.secret } ))
+	.use(opt.static_mount, express.static(opt.static_dir))
     // Template engines
     app.engine('mustache', cons.hogan)
 	.engine('mu', cons.hogan)
 	.set('view engine', 'mustache')
-	.set('views', tmpl_dir);
+	.set('views', opt.template_dir);
     // Custom middleware
-    app.use(exports.logger())
+    app.use(exports.logger(opt.separator))
 	.use(set_cookie)
 	.use(exports.fun_router(app));
 
+    app.start = startServer;
+    
     return app;
 }
 
 /*
-  This function creates a new server using createServer, then 
-  starts it on the given port, or 8080 if no port is given.
+  This function connects to the database (it the app uses one), then
+  starts the server.
 
-  The optional handler, if given, treats any request that is not
-  treated by other handlers. If not given, the default handler
-  returning 404 is used.
+  The port to listen on can be given as first argument.  The second
+  argument is a configuration object for the database, using the same
+  syntax as in the Options object. Both arguments are replaced with
+  the app's options if not given.
 
-  The secret parameter is passed down to createServer.
+  The thid argument is a callback(err) to be executed after the server
+  is started or an error occurs.
 */
-exports.startServer = function(port, handler, secret) {
-    var port = port || 8080;
+function startServer(port, db, next) {
+    var app = this;
+    var port = port || app.get('port');
+    var db = db || app.get('db');
 
-    var app = createServer(secret)
-	.use(handler || exports.default_handler);
-    app.listen(port, function(err) {
-	    if (!err) {
-		console.log("Server started successfully on port", port);
-		console.log();
+    if (!(next instanceof Function)) {
+	next = function(err) {
+	    if (err) console.log(err);
+	}
+    }
+
+    // Starts the server on the requested port
+    // (called after the connection to the database).
+    var db_callback = function(err, db) {
+	if (err) {
+	    next(err);
+	} else {
+	    // this is executed only with the node-sqlite-purejs driver
+	    if (typeof db == 'Sql') {
+		app.db = db;
+		// Use the query escaping facilities of mysql
+		app.db.escape = SqlString.escape;
+		app.db.escapeId = SqlString.escapeId;
+		app.db.format = SqlString.format;
+		// Wrap exec with a signature similar to mysql's
+		// prepared queries
+		app.db.query = function(sql, values, callback) {
+		    if (values instanceof Function) {
+			callback = values;
+		    } else {
+			sql = app.db.format(sql, values);
+		    }
+		    app.db.exec(sql, callback);
+		}
+		app.db.end = function() {};
 	    }
-	});
+
+	    // Add one last handler for unrouted requests
+	    app.use(app.get('default_handler'));
+
+	    // Start the server
+	    app.listen(port, function(err) {
+		    if (err) {
+			next(err);
+		    } else {
+			console.log("Server started successfully on port", port);
+			console.log();
+			next();
+		    }
+		});
+	}
+    }
+
+    // Connect to the database if requested
+    if (db) {
+	if (db.type == 'mysql') {
+	    app.db = mysql.createConnection(db);
+	    app.db.connect(db_callback);
+	} else if (db.type == 'sqlite') {
+	    sqlite.open(db.file, db, db_callback);
+	} else {
+	    next(new Error("Unknown database type: " + db.type));
+	}
+    } else {
+	// Directly start the server
+	db_callback();
+    }
+
     return app;
 }
 
@@ -75,22 +150,27 @@ exports.startServer = function(port, handler, secret) {
 /*
   Default fallback handler. It sends a 404 error.
 */
-exports.default_handler = function(req, res) {
+exports.not_found_handler = function(req, res) {
     res.send(404, "Error 404. Could not find a handler for your request.");
 }
 
 /*
   This middleware logs useful information to the console
 */
-exports.logger = function() {
-    var _done = express.logger(':response-time ms\\n' + sep);
+exports.logger = function(separator) {
+    var sep = separator || '';
+    var _done = express.logger('HTTP/:http-version :status :response-time ms\\n' + sep);
     return function(req, res, next) {
 	_done(req, res, function(err) {
 	    if (err) return next(err);
-	    console.log()
+	    console.log();
 	    console.log(sep);
 	    console.log(Date());
 	    console.log(req.method, req.url, 'HTTP/' + req.httpVersion);
+	    for (h in req.headers) {
+		console.log(h + ': ' + req.headers[h]);
+	    }
+	    console.log();
 	    console.log('req.query (query string):', req.query);
 	    console.log('req.body (POST content):', req.body);
 	    console.log('req.cookies (Cookies):', req.cookies);
