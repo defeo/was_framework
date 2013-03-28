1344e496be96bab2a249a4d3d723a8181bb78236
Luca De Feo <dfl@deagol>
2013-03-28 03:39:30 +0100

    frozen mysql to version 2.0.0-alpha7

--- scripts/SqlString.sh
+++ scripts/SqlString.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -o lib/SqlString.js https://raw.github.com/felixge/node-mysql/master/lib/protocol/SqlString.js 
+curl -o lib/SqlString.js https://raw.github.com/felixge/node-mysql/v2.0.0-alpha7/lib/protocol/SqlString.js
 patch -p0 < scripts/SqlString.patch
